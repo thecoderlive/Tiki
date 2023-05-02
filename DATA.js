2cea5a84e6b2320c9066259cdc1af5a6b74d7739
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-02 19:07:10 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "4c11759b-7903-4b67-82ca-3d72d8b4ab4e",
+    "home_feed": [
+        {
+            "id": "be676c14-c616-4229-a235-cd307b7a6d5c",
+            "artist_name": "trip",
+            "home_carousel": [
+                {
+                    "id": "743dea86-b53b-4660-be05-a7c0880c26cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "ea9da828-8191-4310-87d9-d2ed469fb855",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "4766ee27-28e1-43ad-a2e1-9898a54357e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                },
+                {
+                    "id": "66dc0d35-282b-4ba7-8e08-52ae071cd6a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "e6bd3bb0-57f2-4c14-bddc-51869dce7d59",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                }
+            ],
+            "art_caption": "field",
+            "art_description": "You need behind. Affect since toward onto program."
+        },
+        {
+            "id": "4f78cf3b-0269-473f-b0c0-4ec714ea0d0e",
+            "artist_name": "black",
+            "home_carousel": [
+                {
+                    "id": "0f40f066-f4d3-4817-8b4e-7cb5bd7e427e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "a95301ad-5f8f-404c-b960-a5238a7af408",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+                },
+                {
+                    "id": "ae5a1475-8dc5-4a56-9073-6a517165f8d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "97a7b6d9-8157-4e24-baed-1849429ba126",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "a6abb94d-ea0c-489e-b1e4-ba1ec2e09bb8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                }
+            ],
+            "art_caption": "act",
+            "art_description": "How pass art a."
+        },
+        {
+            "id": "9003e29d-332c-42bf-9e4a-9f1692d6a6bb",
+            "artist_name": "begin",
+            "home_carousel": [
+                {
+                    "id": "c2498410-3a26-45f4-b70b-d0ad355030d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "b8e69a3a-a9cb-439b-9700-b76ac96477e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "39f88c9c-46ac-4ed6-b326-02e233ea382b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "6bcfbf01-635a-479f-b11d-03458261fce1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "06297fba-4eea-456f-bef1-45e1c8640f21",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                }
+            ],
+            "art_caption": "window",
+            "art_description": "Responsibility agree accept guy describe pull available. Doctor strategy PM."
+        },
+        {
+            "id": "0b3eb5dd-f66d-4b13-af1e-1624cfa97c45",
+            "artist_name": "market",
+            "home_carousel": [
+                {
+                    "id": "3697413f-f92d-43c2-9382-437070eb3a4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                },
+                {
+                    "id": "075dd383-8d87-454b-9cff-8bb0b9d32770",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "b8fcec1f-4c18-4f81-9538-f64ed0fa0219",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "b25e1c5d-03d8-4074-bcfd-7112f469a174",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                },
+                {
+                    "id": "f0cf814c-9c7b-41d6-b45d-ba698b8f2db3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                }
+            ],
+            "art_caption": "five",
+            "art_description": "All some view share peace worry. Technology walk husband to me catch learn recent. Sit remember age price attention nature."
+        },
+        {
+            "id": "27f31332-e960-400c-91ba-c2e4bd398d32",
+            "artist_name": "which",
+            "home_carousel": [
+                {
+                    "id": "bcf8c3d2-2c96-42f3-8f5d-7f5aba059b97",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png"
+                },
+                {
+                    "id": "c0ddfbd1-55fb-4891-910d-1a80a61bb533",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "9c2e0756-bfab-4fcc-b8f7-acd908a7e190",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "d7814dc1-ecf1-4655-8c0b-28334cd807d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                },
+                {
+                    "id": "46b92cb2-8aba-46d6-9811-bc59cae97642",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                }
+            ],
+            "art_caption": "dream",
+            "art_description": "Travel industry air down there some onto beat. Wonder control son memory record."
+        }
+    ]
+}
